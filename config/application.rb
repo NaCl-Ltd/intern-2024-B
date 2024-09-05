701773147818762e636b4d30dbb2e99483fbc3ba
TrES_12b <j2104@matsue-ct.ac.jp>
2024-09-05 11:23:40 +0900

    多言語化実装

--- config/application.rb
+++ config/application.rb
@@ -6,5 +6,8 @@ module SampleApp
   class Application < Rails::Application
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
