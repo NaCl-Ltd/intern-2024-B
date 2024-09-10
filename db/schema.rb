fb5e3cbe2d7cdbb0c4aa09090aecb3bfed397ab2
TrES_12b <j2104@matsue-ct.ac.jp>
2024-09-10 11:54:18 +0900

    ニックネームの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_06_023244) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_10_012046) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_06_023244) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
